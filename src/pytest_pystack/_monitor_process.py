c76ea1880e4ef5dc10f7936cc9173253d18c7b8f
Matt Wozniski <mwozniski@bloomberg.net>
2026-06-07 04:07:56 -0400

    Use the "spawn" start method unconditionally
    Rather than trying to support both "fork" and "spawn", force "spawn" to
    be used no matter what.
    
    Split the monitor process's entry point out into a separate module to
    avoid having a global multiprocessing `Queue` object get created when it
    is reimported in the spawned process, as otherwise Python 3.8 and 3.9
    report that some semaphores are leaked by the child process.

--- src/pytest_pystack/_monitor_process.py
+++ src/pytest_pystack/_monitor_process.py
@@ -1,11 +1,7 @@
 import os
-import shlex
-import subprocess
-import sys
-from queue import Empty
 
-from ._config import PystackConfig
 from ._debug_detect import debug_detected
+from ._monitor import monitor
 from ._multiprocessing_context import MP_CTX
 
 _queue = MP_CTX.Queue()
@@ -15,7 +11,7 @@ _process = None
 def start(config):
     global _process
     _process = MP_CTX.Process(
-        target=_run_monitor,
+        target=monitor,
         args=(
             config,
             os.getpid(),
@@ -34,50 +30,3 @@ def stop():
         _process.join(timeout=5)
         if _process.is_alive():
             _process.kill()
-
-
-def _run_monitor(config: PystackConfig, pid, queue, debug_detected):
-    pystack_cmd = [
-        config.pystack_path,
-        "remote",
-    ]
-    if config.pystack_args:
-        pystack_cmd += shlex.split(config.pystack_args)
-    handled_test_cases = set()
-    while True:
-        testcase = queue.get()
-        if testcase is None:
-            break
-
-        if testcase in handled_test_cases:
-            continue
-        handled_test_cases.add(testcase)
-
-        try:
-            new_testcase = queue.get(timeout=config.threshold)
-            if new_testcase != testcase:
-                print(
-                    f"new test {new_testcase} should not start before previous {testcase} test finished",
-                    file=sys.__stderr__,
-                )
-                raise Exception(
-                    "new test should not start before previous test finished"
-                )
-        except Empty:
-            output = ""
-            output += f"\n\n**** PYSTACK  -- {testcase} ***\n"
-            output += f"Timed out waiting for process {pid} to finish {testcase}:"
-            proc = subprocess.run(
-                [*pystack_cmd, str(pid)],
-                stdout=subprocess.PIPE,
-                text=True,
-            )
-            output += proc.stdout
-            output += "**** PYSTACK  ***\n"
-            is_debug = debug_detected.is_set()
-            debug_detected.clear()
-            if config.print_stderr and not is_debug:
-                print(output, file=sys.__stderr__)
-            if config.output_file:
-                with open(config.output_file, "a") as f:
-                    print(output, file=f)
